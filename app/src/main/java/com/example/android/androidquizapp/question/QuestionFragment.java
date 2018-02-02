2a3c14055a5242bff1a2a1a174f699ad4ef82b2d
Fábio Gouveia <hpgroods@gmail.com>
2018-02-02 05:59:40 +0000

    feat : improve constructTextualAnswerPanelLayout method
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/androidquizapp/question/QuestionFragment.java
+++ app/src/main/java/com/example/android/androidquizapp/question/QuestionFragment.java
@@ -9,12 +9,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
@@ -29,6 +32,7 @@ import com.example.android.androidquizapp.level.Level;
 import com.example.android.androidquizapp.level.LevelSelectionActivity;
 import com.example.android.androidquizapp.utils.DialogUtils;
 import com.example.android.androidquizapp.utils.QueryUtils;
+import com.example.android.androidquizapp.utils.SoundUtils;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
@@ -47,7 +51,11 @@ public class QuestionFragment extends Fragment {
     //Question fragment class members
     private static final String QUESTION_INDEX_KEY = "question_index_key";
 
+    //Utilities for sound
+    private SoundUtils soundUtils;
+
     //Question fragment instance members.
+    private View rootView;
     private Level level;
     private Question question;
     private int questionIndex;
@@ -55,6 +63,7 @@ public class QuestionFragment extends Fragment {
     private TextView multipleChoiceQuestionHint, passedQuestionLabel;
     private RadioGroup oneChoiceAnswerRadioGroup;
     private LinearLayout checkBoxesHolder;
+    private TextInputLayout answerTextInputLayout;
     private TextInputEditText textualAnswerInputField;
     private Button answerButton;
     private boolean[] answersState;//For non textual answers
@@ -64,7 +73,10 @@ public class QuestionFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.question_fragment, container, false);
+        rootView = inflater.inflate(R.layout.question_fragment, container, false);
+
+        //Get sound utilities
+        soundUtils = new SoundUtils(getActivity());
 
         if(savedInstanceState != null){
             savedInstanceState.setClassLoader(Level.class.getClassLoader());
@@ -74,19 +86,16 @@ public class QuestionFragment extends Fragment {
             question = savedInstanceState.getParcelable(Question.QUESTION_KEY);
         }
 
-
         //Initialize our views
         TextView questionTextView = rootView.findViewById(R.id.question_text_view);
         multipleChoiceQuestionHint = rootView.findViewById(R.id.multiple_choice_question_hint_textView);
         oneChoiceAnswerRadioGroup = rootView.findViewById(R.id.answers_radio_group);
         checkBoxesHolder = rootView.findViewById(R.id.checkBoxes_holder);
+        answerTextInputLayout = rootView.findViewById(R.id.answer_text_input_layout);
         textualAnswerInputField = rootView.findViewById(R.id.textual_answer_text_input_field);
         answerButton = rootView.findViewById(R.id.answers_btn);
         passedQuestionLabel = rootView.findViewById(R.id.passed_question_label_text_view);
 
-        if(question.passed()){
-            questionPassed();
-        }
 
         questionTextView.setText(question.getQuestion());
 
@@ -94,6 +103,10 @@ public class QuestionFragment extends Fragment {
 
         //Start the answer fragment layout construction.
         startAnswerPanelConstruction();
+
+        if (question.passed()) {
+            showQuestionPassedLayoutMode();
+        }
 
         //Add an on click listener to the button so the user can check the answer.
         answerButton.setOnClickListener(new View.OnClickListener() {
@@ -107,14 +120,33 @@ public class QuestionFragment extends Fragment {
         return rootView;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
 
     @Override
     public void onStop() {
         super.onStop();
+
+        //Release the audio
+        soundUtils.releaseMedia();
+    }
+
+    /*
+        This method is used exclusively to hide the hard key board from the screen
+        if the user scroll the view pager without explicitly hide the key board.
+     */
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+
+        //Get input method manager system service
+        InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputMethodManager != null) {
+
+            //Hide key board
+            inputMethodManager.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
+        }
+
+        return super.onCreateAnimation(transit, enter, nextAnim);
+
     }
 
     /**
@@ -160,6 +192,7 @@ public class QuestionFragment extends Fragment {
             possibleAnswer = (CheckBox) checkBoxesHolder.getChildAt(i);
             possibleAnswer.setText(possibleAnswers[i]);
             if(question.passed()){
+                possibleAnswer.setEnabled(question.getRightAnswersState()[i]);
                 possibleAnswer.setChecked(question.getRightAnswersState()[i]);
             }else{
                 possibleAnswer.setOnClickListener(new View.OnClickListener() {
@@ -179,31 +212,46 @@ public class QuestionFragment extends Fragment {
 
     //This method constructs a textual answer panel and shows it on this fragment layout.
     private void constructTextualAnswerPanelLayout(){
-        textualAnswerInputField.setVisibility(View.VISIBLE);
+        answerTextInputLayout.setVisibility(View.VISIBLE);
 
-        textualAnswerInputField.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if (question.passed()) {
+            //Format final answer string
+            String userRightAnswer = getResources().getString(R.string.user_final_correct_textual_answer, question.getTextualUserAnswer());
 
-                //If the user presses down a key
-                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            //Hide the hint
+            answerTextInputLayout.setHintEnabled(false);
+            textualAnswerInputField.setText(userRightAnswer);
+            textualAnswerInputField.setGravity(Gravity.CENTER_HORIZONTAL);
+            textualAnswerInputField.setEnabled(false);
+        } else {
 
-                    //If enter key was pressed
-                    if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            textualAnswerInputField.setOnKeyListener(new View.OnKeyListener() {
+                @Override
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
 
-                        //Check user answer
-                        checkAnswer();
-                        return true;
-                    } else {
-                        return false;
+                    //If the user presses down a key
+                    if (event.getAction() == KeyEvent.ACTION_DOWN) {
+
+                        //If enter key was pressed
+                        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+
+                            //Check user answer
+                            checkAnswer();
+                            return true;
+                        } else {
+                            return false;
+                        }
                     }
+                    return false;
                 }
-                return false;
-            }
-        });
+            });
+        }
     }
 
-    //This method constructs a one choice radio button panel for the answers and shows it on this fragment layout.
+
+    /**
+     * This method constructs a one choice radio button panel for the answers and shows it on this fragment layout
+     **/
     private void constructOneChoiceAnswerPanelLayout(){
         final int numPossibleAnswers = question.getPossibleAnswersLength();
         answersState = new boolean[numPossibleAnswers];
@@ -214,7 +262,8 @@ public class QuestionFragment extends Fragment {
         for (int i = 0; i < numPossibleAnswers; i++) {
             possibleAnswer = (RadioButton) oneChoiceAnswerRadioGroup.getChildAt(i);
             possibleAnswer.setText(possibleAnswers[i]);
-            if(question.passed()){
+            if (question.passed()) {
+                possibleAnswer.setEnabled(question.getRightAnswersState()[i]);
                 possibleAnswer.setChecked(question.getRightAnswersState()[i]);
             }else {
                 possibleAnswer.setOnClickListener(new View.OnClickListener(){
@@ -231,44 +280,85 @@ public class QuestionFragment extends Fragment {
         }
     }
 
+    /**
+     * This method checks an answer
+     **/
     private void checkAnswer() {
         if (question.getType() == Question.Type.TEXTUAL && question.checkAnswer(getActivity().getApplicationContext(), level, questionIndex, textualAnswerInputField.getText().toString())) {
-            questionPassed();
+            showQuestionPassedLayoutMode();
+
+            //Go to the next question
+            nextQuestion();
+
             if (level.isPassed()) {
+                //Show level passed notification
                 showLevelPassedNotification();
             }
-            nextQuestion();
         } else if (question.getType() != Question.Type.TEXTUAL && question.checkAnswer(getActivity().getApplicationContext(), level, questionIndex, answersState)) {
-            questionPassed();
+            showQuestionPassedLayoutMode();
+
+            //Go to the next question
+            nextQuestion();
+
             if (level.isPassed()) {
+                //Show level passed notification
                 showLevelPassedNotification();
             }
-            nextQuestion();
         } else {
-            //Wrong answer, show a tip to help the user and increment static wrong answers on QuestionActivity.
+            //Play wrong answer sound
+            soundUtils.playSound(Question.Sound.WRONG_ANSWER);
+
+            //Wrong answer, show a tip to help the user and increment static wrong answers on QuestionActivity
             showTip();
             QuestionsActivity.NUMBER_OF_WRONG_ANSWERS++;
         }
     }
 
-    //Prepare the layout when the question is passed.
-    private void questionPassed(){
+    /**
+     * This method turns the layout to passed mode
+     **/
+    private void showQuestionPassedLayoutMode(){
         answerButton.setVisibility(View.GONE);
         passedQuestionLabel.setVisibility(View.VISIBLE);
     }
 
-    //Change to the next question page when the user passes a question.
+    /** This method plays a sound, updates the layout and sends the user to the next question **/
     private void nextQuestion() {
-        ((QuestionsActivity) this.getActivity()).viewPagerNextQuestion();
+
+        //Play a sound
+        soundUtils.playSound(Question.Sound.CORRECT_ANSWER);
+
+        //Notify questions adapter of data changes
+        ((QuestionsActivity) getActivity()).viewPagerRefreshQuestionLayout();
+
+        //Wait half a second before send the user to the next question
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Go to the next question
+                ((QuestionsActivity) getActivity()).viewPagerNextQuestion();
+            }
+        }, 500);
     }
 
-    //Show a tip to help answering the question.
+    /**
+     * This method refresh the layout and plays a sound when this question is changed
+     **/
+    public void questionChanged() {
+        startAnswerPanelConstruction();
+    }
+
+    /** This method shows a tip to help answering the question **/
     private void showTip() {
 
+        //Get the layout inflater
         LayoutInflater inflater = getLayoutInflater();
 
+        //Get te root view group to use in inflater, inflate
+        ViewGroup failedQuestionRoot = getActivity().findViewById(R.id.question_fragment_layout);
+
         //Inflate custom popup window layout
-        View failedQuestionToastRoot = inflater.inflate(R.layout.failed_question_toast, null);
+        View failedQuestionToastRoot = inflater.inflate(R.layout.failed_question_toast, failedQuestionRoot, false);
 
         //Find the failed image view on the inflated layout
         ImageView alertIcon = failedQuestionToastRoot.findViewById(R.id.toast_failed_image_view);
@@ -305,12 +395,13 @@ public class QuestionFragment extends Fragment {
         }, 3000);
     }
 
-    //This method shows a notification when the level is passed for the first time.
+    /** This method shows a notification when the level is passed for the first time **/
     private void showLevelPassedNotification() {
 
         //Get preferences
         SharedPreferences preferences = getActivity().getSharedPreferences(QueryUtils.PREFERENCES_FILE, Context.MODE_PRIVATE);
 
+        //Create a notification key
         String NOTIFICATION_KEY = level.getName().toLowerCase() + "_notification_already_showed";
 
         //If level passed notification was already showed don't show it again.
@@ -318,12 +409,17 @@ public class QuestionFragment extends Fragment {
             return;
         }
 
+        //Play a sound for level passed alert
+        soundUtils.playSound(Question.Sound.LEVEL_PASSED);
+
+        //Get shared preferences editor and edit preferences
         SharedPreferences.Editor prefsEditor = preferences.edit();
         prefsEditor.putBoolean(NOTIFICATION_KEY, true);
+
+        //Apply the changes
         prefsEditor.apply();
 
-
-        //Prepare de bundle with dialog arguments
+        //Prepare a bundle with dialog arguments
         Bundle levelPassedArguments = new Bundle();
         levelPassedArguments.putString(DialogUtils.LEVEL_EARNINGS_KEY, level.getEarnings());
 
@@ -341,7 +437,7 @@ public class QuestionFragment extends Fragment {
 
         //Build the notification.
         Notification levelPassedNotification = new Notification.Builder(getContext())
-                .setSmallIcon(R.drawable.ic_failed_beginner_question)
+                .setSmallIcon(R.drawable.ic_level_done)
                 .setContentTitle(getResources().getString(R.string.level_passed_dialog_title))
                 .setStyle(new Notification.BigTextStyle().bigText(String.format(getResources().getString(R.string.level_passed_dialog_msg), level.getEarnings())))
                 .setContentIntent(pIntent).build();
@@ -359,8 +455,7 @@ public class QuestionFragment extends Fragment {
 
     }
 
-
-    //Any moment the app dismiss from the screen, save the state of your instance.
+    /** Any moment the app dismiss from the screen, save this instance state **/
     @Override
     public void onSaveInstanceState(Bundle outState) {
         //If clickable toast exists, dismiss
